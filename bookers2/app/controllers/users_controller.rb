cfd73ecdc0920e225d7510295d6f4815213e40c9
EC2 Default User <ec2-user@ip-172-31-17-6.ap-northeast-1.compute.internal>
2023-07-06 11:17:05 +0000

    [finish] check sheet 38/57

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
   
   def show
@@ -11,4 +18,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
   
+  private
+  def user_params
+    params.require(:user).permit(:name, :image, :info)
+  end
 end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:new, :index, :show, :create, :destroy, :edit, :update]
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :update]
   get "about" => 'homes#about'
 end
